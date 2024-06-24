bb6c051cee6d31e130180e8abf8ddc97c74f60f1
damoncrockett <damoncrockett@hotmail.com>
2024-06-23 23:47:57 -0400

    pre-records

--- manufacturers.py
+++ manufacturers.py
@@ -21,6 +21,12 @@ os.makedirs(output_dir, exist_ok=True)
 
 man_file_path = 'manufacturers.jsonl'
 bran_file_path = 'brands.jsonl'
+surf_file_path = 'surfaces.jsonl'
+backp_file_path = 'backprints.jsonl'
+xd_file_path = 'texture_descriptors.jsonl'
+gd_file_path = 'gloss_descriptors.jsonl'
+cd_file_path = 'color_descriptors.jsonl'
+td_file_path = 'thickness_descriptors.jsonl'
 
 with open(man_file_path, 'w') as jsonl_file:
     for man in mans:
@@ -69,7 +75,7 @@ with open(man_file_path, 'w') as jsonl_file:
 
             brand = model.Type(ident=f'https://paperbase.xyz/records/{bransafe}', label=bran)
             brand.identified_by = vocab.PrimaryName(content=bran)
-            brand.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+            brand.broader = model.Type(ident="http://vocab.getty.edu/aat/300389735", label="Brand Name Objects")
             brand.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300028776", label="Trademark")
 
             cre = model.Creation()
@@ -89,6 +95,189 @@ with open(man_file_path, 'w') as jsonl_file:
 
             # Write to individual JSON file
             filename = bransafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # surfaces under this man
+        surfs = thisman.s.loc[thisman.s.notnull()].unique()
+        surfs = [item for item in surfs if item != '[not specified]']
+
+        for surf in surfs:
+            thissurf = thisman.loc[thisman.s == surf]
+
+            surfsafes = thissurf.s.unique()
+            assert len(surfsafes) == 1
+            surfsafe = f'SURF_{surfsafes[0]}_MAN_{mansafes[0]}.json'
+
+            surface = model.Type(ident=f'https://paperbase.xyz/records/{surfsafe}', label=surf)
+            surface.identified_by = vocab.PrimaryName(content=surf)
+            surface.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            surface.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(surface)
+            add_record_to_jsonl(js, surf_file_path)
+
+            # Write to individual JSON file
+            filename = surfsafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # backprints under this man
+        backps = thisman.backp.loc[thisman.backp.notnull()].unique()
+
+        for backp in backps:
+            thisbackp = thisman.loc[thisman.backp == backp]
+
+            backpsafes = thisbackp.backp.unique()
+            assert len(backpsafes) == 1
+            backpsafe = f'BACKP_{backpsafes[0]}_MAN_{mansafes[0]}.json'
+
+            backprint = model.Type(ident=f'https://paperbase.xyz/records/{backpsafe}', label=f'{man} {backp}')
+            backprint.identified_by = vocab.PrimaryName(content=f'{man} {backp}')
+            backprint.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300438445", label="Wordmark")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            backprint.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(backprint)
+            add_record_to_jsonl(js, backp_file_path)
+
+            # Write to individual JSON file
+            filename = backpsafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # texture descriptors under this man
+        xds = thisman.xd.loc[thisman.xd.notnull()].unique()
+        xds = [item for item in xds if item != '[texture unspecified]']
+
+        for xd in xds:
+            thisxd = thisman.loc[thisman.xd == xd]
+
+            xdsafes = thisxd.xd.unique()
+            assert len(xdsafes) == 1
+            xdsafe = f'XD_{xdsafes[0]}_MAN_{mansafes[0]}.json'
+
+            texture_descriptor = model.Type(ident=f'https://paperbase.xyz/records/{xdsafe}', label=xd)
+            texture_descriptor.identified_by = vocab.PrimaryName(content=xd)
+            texture_descriptor.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+            texture_descriptor.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300056362", label="Texture")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            texture_descriptor.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(texture_descriptor)
+            add_record_to_jsonl(js, xd_file_path)
+
+            # Write to individual JSON file
+            filename = xdsafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # gloss descriptors under this man
+        gds = thisman.gd.loc[thisman.gd.notnull()].unique()
+        gds = [item for item in gds if item != '[gloss unspecified]']
+
+        for gd in gds:
+            thisgd = thisman.loc[thisman.gd == gd]
+
+            gdsafes = thisgd.gd.unique()
+            assert len(gdsafes) == 1
+            gdsafe = f'GD_{gdsafes[0]}_MAN_{mansafes[0]}.json'
+
+            gloss_descriptor = model.Type(ident=f'https://paperbase.xyz/records/{gdsafe}', label=gd)
+            gloss_descriptor.identified_by = vocab.PrimaryName(content=gd)
+            gloss_descriptor.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+            gloss_descriptor.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300179475", label="Reflectance")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            gloss_descriptor.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(gloss_descriptor)
+            add_record_to_jsonl(js, gd_file_path)
+
+            # Write to individual JSON file
+            filename = gdsafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # color descriptors under this man
+        cds = thisman.cd.loc[thisman.cd.notnull()].unique()
+        cds = [item for item in cds if item != '[base color unspecified]']
+
+        for cd in cds:
+            thiscd = thisman.loc[thisman.cd == cd]
+
+            cdsafes = thiscd.cd.unique()
+            assert len(cdsafes) == 1
+            cdsafe = f'CD_{cdsafes[0]}_MAN_{mansafes[0]}.json'
+
+            color_descriptor = model.Type(ident=f'https://paperbase.xyz/records/{cdsafe}', label=cd)
+            color_descriptor.identified_by = vocab.PrimaryName(content=cd)
+            color_descriptor.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+            color_descriptor.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300080438", label="Color")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            color_descriptor.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(color_descriptor)
+            add_record_to_jsonl(js, cd_file_path)
+
+            # Write to individual JSON file
+            filename = cdsafe
+            file_path = os.path.join(output_dir, filename)
+            with open(file_path, 'w') as individual_file:
+                rec = json.dumps(js)
+                individual_file.write(rec)
+
+        # thickness descriptors under this man
+        tds = thisman.td.loc[thisman.td.notnull()].unique()
+        tds = [item for item in tds if item != '[weight unspecified]']
+
+        for td in tds:
+            thistd = thisman.loc[thisman.td == td]
+
+            tdsafes = thistd.td.unique()
+            assert len(tdsafes) == 1
+            tdsafe = f'TD_{tdsafes[0]}_MAN_{mansafes[0]}.json'
+
+            thickness_descriptor = model.Type(ident=f'https://paperbase.xyz/records/{tdsafe}', label=td)
+            thickness_descriptor.identified_by = vocab.PrimaryName(content=td)
+            thickness_descriptor.broader = model.Type(ident="http://vocab.getty.edu/aat/300375551", label="Brand Name Materials")
+            thickness_descriptor.classified_as = model.Type(ident="http://vocab.getty.edu/aat/300056240", label="Weight")
+
+            cre = model.Creation()
+            cre.carried_out_by = manufacturer
+            thickness_descriptor.created_by = cre
+
+            # Convert to JSON
+            js = model.factory.toJSON(thickness_descriptor)
+            add_record_to_jsonl(js, td_file_path)
+
+            # Write to individual JSON file
+            filename = tdsafe
             file_path = os.path.join(output_dir, filename)
             with open(file_path, 'w') as individual_file:
                 rec = json.dumps(js)
